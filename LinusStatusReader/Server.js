3a8e5a2a20991ac3e79acdf7381456999f115dfb
Markus Pokkinen <markuspokkinen@gmail.com>
2020-02-05 21:48:26 +0200

    Start

--- LinusStatusReader/Server.js
+++ LinusStatusReader/Server.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const fs = require("fs");
+const express = require("express");
+
 const mockFile = "./assets/Status.txt";
 const ubuntuFile = "../../../../var/lib/dpkg/status";
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.static(__dirname + "/views"));
+
+app.listen(port);
+console.log("Server running in port: " + port);
 
 function bufferFile(filepath) {
     return fs.readFileSync(filepath);
