059da4074deb0efe9db210972cabdd5d3d6fbccd
Markus Pokkinen <markuspokkinen@gmail.com>
2020-02-05 22:41:20 +0200

    multiple folders

--- LinusStatusReader/Server.js
+++ LinusStatusReader/Server.js
@@ -14,6 +14,17 @@ app.use(express.static(__dirname + "/views"));
 app.listen(port);
 console.log("Server running in port: " + port);
 
+app.get("/file", (req, res) => {
+    let fullData;
+
+    if (fs.existsSync(ubuntuFile)) {
+        fullData = bufferFile(ubuntuFile).toString("ascii").split("\r\n\r\n").map((val) => val.split("\r\n"));
+    } else {
+        fullData = bufferFile(mockFile).toString("ascii").split("\r\n\r\n").map((val) => val.split("\r\n"));
+    }
+    res.json(fullData);
+});
+
 function bufferFile(filepath) {
     return fs.readFileSync(filepath);
 }
