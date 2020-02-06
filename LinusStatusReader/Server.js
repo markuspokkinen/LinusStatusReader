932a4f75b76d75d3eaf623b0013977413f4fb0f2
Markus Pokkinen <markuspokkinen@gmail.com>
2020-02-06 14:27:07 +0200

    path and jsoning fixes

--- LinusStatusReader/Server.js
+++ LinusStatusReader/Server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const express = require("express");
 
 const mockFile = "./assets/Status.txt";
-const ubuntuFile = "../../../../var/lib/dpkg/status";
+const ubuntuFile = "../../../../../../../var/lib/dpkg/status";
 
 const port = process.env.PORT || 8080;
 
@@ -18,24 +18,30 @@ app.get("/file", (req, res) => {
     let fullData;
 
     if (fs.existsSync(ubuntuFile)) {
-        fullData = bufferFile(ubuntuFile).toString("ascii").split("\r\n\r\n");
+        fullData = bufferFile(ubuntuFile).toString("ascii").split("\n\n").map(val => ubuntuFileToOneJson(val));
         console.log("unbuntufile");
     } else {
-        fullData = bufferFile(mockFile).toString("ascii").split("\r\n\r\n");   
+        fullData = bufferFile(mockFile).toString("ascii").split("\r\n\r\n").map(val => mockFileToOneJson(val));   
         console.log("mockfile");
     }
     fullData.sort();
-    let fullJson = fullData.map(val => fileToOneJson(val));
-    //console.log(fullJson);
-    res.json(fullJson);
+    res.json(fullData);
 });
 
 function bufferFile(filepath) {
     return fs.readFileSync(filepath,'UTF-8');
 }
-function fileToOneJson(onePackage) {
+function mockFileToOneJson(onePackage) {
     let packageName = onePackage.substring(onePackage.indexOf("Package:"), onePackage.indexOf("\r\n", onePackage.indexOf("Package:")));
     let depends = onePackage.substring(onePackage.indexOf("Depends:"), onePackage.indexOf("\r\n", onePackage.indexOf("Depends:")));
+    if (depends === packageName) depends = "";
+    let description = onePackage.substring(onePackage.indexOf("Description:"));
+    return { packageName, depends, description };
+}
+function ubuntuFileToOneJson(onePackage) {
+    let packageName = onePackage.substring(onePackage.indexOf("Package:"), onePackage.indexOf("\n", onePackage.indexOf("Package:")));
+    let depends = onePackage.substring(onePackage.indexOf("Depends:"), onePackage.indexOf("\n", onePackage.indexOf("Depends:")));
+    if (depends === packageName) depends = "";
     let description = onePackage.substring(onePackage.indexOf("Description:"));
     return { packageName, depends, description };
 }
